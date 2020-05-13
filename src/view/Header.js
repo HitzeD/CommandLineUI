1c75b42d5cc3ba94a0b48bdf3ec72014b36fc4c5
HitzeD <dakotahhitze@gmail.com>
2020-05-13 19:55:12 -0400

    imported navbar

---
+++ src/view/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../components/Navbar.js';
+
+
+const Header = () => {
+    return(
+        <div>
+
+            <Navbar />
+
+
+
+        </div>
+    )
+}
+
+export default Header;
