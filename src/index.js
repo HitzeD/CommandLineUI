27d775d1bd01005a792be03f83e4ee7b37a024c4
HitzeD <dakotahhitze@gmail.com>
2020-05-13 20:03:27 -0400

    moved index.scss to scss folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, withRouter} from 'react-router-dom';
 
-import './index.scss';
+import './scss/general/index.scss';
 import App from './App';
 
 const AppwithRouter = withRouter(App);
